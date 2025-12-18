28ca940242db4b0a5ce4fe29d2203af2ccc461b7
soubhagya-m-dev <soubhagya.m2024@gmail.com>
2025-12-18 08:48:27 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as a newer version of js
+
+// alert("Jello") //we are using node js, not browser 
+
+//data types
+/*
+number => 2 to the power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value, value doesn't come
+undefined => value not defined
+symbol => unique
+*/
+let age = 15
+let names = ""
+let isLoggedIn = false
+let temp = null
+let state
+console.table([age, names, isLoggedIn, temp, state])
+
+console.log(typeof null) // type is object - interview qs.
+console.log(typeof undefined) // type is undefined
