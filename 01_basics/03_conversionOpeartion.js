7619f6eabe041d2fe85e6d0d2bfab83fb7930ee4
soubhagya-m-dev <soubhagya.m2024@gmail.com>
2025-12-20 06:55:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOpeartion.js
@@ -0,0 +1,69 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber = Number(score) //string to number conversiom
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+// for NaN case
+let score1 = "33a"
+console.log(typeof score1)
+let valueInNumber1 = Number(score1) //string to number conversiom
+console.log(valueInNumber1); //NaN
+console.log(typeof valueInNumber1);
+console.log(typeof NaN); //NaN is also a number
+
+
+//for null case
+console.log("For null case");
+let score2 = null
+console.log(typeof score2)
+let valueInNumber2 = Number(score2) //null to number conversiom
+console.log(valueInNumber2); //0
+console.log(typeof valueInNumber2); //number
+
+
+//undefined case
+console.log("for undefined case");
+let score3 = undefined
+console.log(typeof score3)
+let valueInNumber3 = Number(score3) //undefined to number conversiom
+console.log(valueInNumber3);
+console.log(typeof valueInNumber3);
+
+
+//boolean case
+console.log("for boolean case");
+let score4 = false
+console.log(typeof score4)
+let valueInNumber4 = Number(score4) //boolean to number conversion true=1 false=0
+console.log(valueInNumber4);
+console.log(typeof valueInNumber4);
+
+//Number conversion => Number()
+// "33" => 33
+// "33abc" => NaN type:Number
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+
+console.log("Boolean Conversion");
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn, "type :",typeof booleanIsLoggedIn);
+
+// 1 => true , 0 => "false"
+// "" => false
+// "sam" => true , " " => true
+
+console.log("String conversion")
+
+let someNumber = 39
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
