2cdbc5d5b89f18186eb50dc2abe753a9907a5de9
soubhagya-m-dev <soubhagya.m2024@gmail.com>
2025-12-31 04:40:01 +0000

    commit

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2">1);
+
+// console.log(null > 0); //always avoid null and undefined type of
+// console.log(null == 0); //comparision this type of comparison cretaes
+// console.log(null === 0); //inconsistency
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
