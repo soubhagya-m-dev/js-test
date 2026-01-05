aad75c1597e5cf07e43e068aed2f290fe786be8d
soubhagya-m-dev <soubhagya.m2024@gmail.com>
2026-01-05 11:46:19 +0000

    date

---
+++ 01_basics/08_datesIn.js
@@ -0,0 +1,14 @@
+//Dates
+//Date is a object in js
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+
+
+
+
