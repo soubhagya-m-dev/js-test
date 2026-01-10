7e6fdd593eba8b8d64db15232e6a8b99e1dcd070
soubhagya-m-dev <soubhagya.m2024@gmail.com>
2026-01-10 07:08:17 +0000

    object sec lec

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,5 +24,54 @@ const regularUser = {
 }
 //console.log(regularUser.fullname.username.firstname);
 
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "c", 4 : "d"}
+const obj3 = {5 : "e", 6 : "f"}
+
+//const obj3 = {obj1, obj2}
+//console.log(obj3);
+
+
+//assign method
+const obj4 = Object.assign({}, obj1, obj2, obj3)
+//in assign first object is target object all the other object paste values on it, for this there are always use a empty object at the initial position in assign method
+//console.log(obj4);
+//console.log(obj1);
+
+//spread operator
+const obj5 = {...obj1, ...obj2, ...obj3} //more userfriendly than assign
+//console.log(obj5);
+
+
+//getting the info of object in arrays
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); //checks a object has a mentioned property or not
+
+
+//getting values from database, multiple values in a array
+const users = [
+    {
+        id : 1,
+        email : "S@gmail.com"
+    },
+    {
+        id : 2,
+        email : "M@gmail.com"
+    }
+]
+// console.log(users[0].email);
+
+
+
+
+
+
+
+
+
+
 
 
